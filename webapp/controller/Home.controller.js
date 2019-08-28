29caabaa98324fda2d94eadbbcf7a6b9c0aa0420
wimpykid26 <dwaipayan2609@gmail.com>
2019-08-28 13:49:53 +0530

    Initial commit

---
+++ webapp/controller/Home.controller.js
@@ -0,0 +1,20 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+	"../model/formatter",
+	"sap/m/MessageToast"
+], function (Controller, formatter, MessageToast) {
+	"use strict";
+
+	return Controller.extend("sap.ui.demo.basicTemplate.controller.App", {
+
+		formatter: formatter,
+
+		onInit: function () {
+
+		},
+
+		onShowHello: function () {
+			MessageToast.show('Hello OpenSAP!');
+		}
+	});
+});
